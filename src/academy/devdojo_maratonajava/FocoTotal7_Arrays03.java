ea506849d2537d209ce8f02b85c1022cdedf0303
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2024-05-09 23:53:19 -0300

    Arrays pt3

---
+++ src/academy/devdojo_maratonajava/FocoTotal7_Arrays03.java
@@ -0,0 +1,19 @@
+package academy.devdojo_maratonajava;
+
+public class FocoTotal7_Arrays03 {
+    public static void main(String[] args) {
+        int [] numeros = new int[3];
+        int [] numeros2 = {1, 2, 3, 4, 5};
+        int [] numeros3 = new int[]{5, 4, 3, 2, 1};
+
+//        for (int i = 0;i < numeros2.length;i++){
+//            System.out.println(numeros2[i]);
+//        }
+
+
+    // foreach
+        for (int num:numeros3){
+            System.out.println(num);
+        }
+    }
+}
