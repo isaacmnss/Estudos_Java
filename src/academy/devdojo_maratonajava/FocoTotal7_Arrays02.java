2cc0e9d9c1f67b02d96a1aa2280e29a3dd9854a8
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2024-05-09 23:30:24 -0300

    Arrays.Pt2

---
+++ src/academy/devdojo_maratonajava/FocoTotal7_Arrays02.java
@@ -0,0 +1,26 @@
+package academy.devdojo_maratonajava;
+
+import java.util.stream.Stream;
+
+public class FocoTotal7_Arrays02 {
+    public static void main(String[] args) {
+        // byte, short, int, long, double
+        // char '\u0000 '''
+        // boolean false
+        // String null
+
+        String [] nomes = new String[4];
+        nomes[0] = "Isaac";
+        nomes[1] = "Gisele";
+        nomes[2] = "Fabricio";
+        nomes[3] = "Marli";
+
+        for (int i = 0; i < nomes.length; i++){
+            System.out.println(nomes[i]);
+        }
+        nomes = new String[5];
+        System.out.println(nomes[1]);
+
+
+    }
+}
