a06df2baac50f1cb279b94bc26695f34727ff49e
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2024-06-07 16:57:21 -0300

    arrays multidimensionais pt.2

---
+++ src/academy/devdojo_maratonajava/FocoTotal_ArraysMultidimensionais02.java
@@ -0,0 +1,29 @@
+package academy.devdojo_maratonajava;
+
+public class FocoTotal_ArraysMultidimensionais02 {
+    public static void main(String[] args) {
+        int[][] arrayInt = new int[3][];
+        int[] array = {1, 2, 3};
+
+        arrayInt[0] = new int[2];
+        // array inicializado com valor padrão de arrays tipo int (0)
+        arrayInt[1] = array;
+        // array inicializado fazendo referência a outro array, na linha 6, onde foram determinados os valores de cada posição.
+        arrayInt[2] = new int[]{1, 2, 3, 4, 5, 6};
+        // array inicializado com posições determinadas na mesma linha
+
+        int[][] arrayInt2 = {{0, 0}, {1, 2, 3, 4}, {1, 2, 3, 4, 5, 6}};
+        // array multidimensional inicializado com posições determinadas na mesma linha
+
+        for (int[] arrayBase : arrayInt) {
+            System.out.println("\n-----");
+            for (int num : arrayBase)
+                System.out.println(num + " ");
+        }
+        for (int[] arrayBase : arrayInt2) {
+            System.out.println("\n-----");
+            for (int num : arrayBase)
+                System.out.println(num + " ");
+        }
+    }
+}
