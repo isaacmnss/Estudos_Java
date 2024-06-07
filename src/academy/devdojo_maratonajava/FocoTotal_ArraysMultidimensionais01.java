3f45f94c48ec4f2b8d52e9414e0da236c278a879
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2024-06-07 01:28:43 -0300

    adaaa

---
+++ src/academy/devdojo_maratonajava/FocoTotal_ArraysMultidimensionais01.java
@@ -0,0 +1,9 @@
+package academy.devdojo_maratonajava;
+
+public class FocoTotal_ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        // 1,2,3,4,5 meses
+        //31,28,31,30 dias
+        int [] [] dias = new int [12] [];
+    }
+}
