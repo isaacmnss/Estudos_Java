ed2ed5cf474eea16661ff13f5fcf56dc4d65cef7
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2024-06-08 04:29:17 -0300

    Exercícios de POO/Classes

---
+++ src/academy/devdojo_maratonajava/javacore/Aintroduçãoclasses/test/CarroTest01.java
@@ -0,0 +1,19 @@
+package academy.devdojo_maratonajava.javacore.Aintroduçãoclasses.test;
+
+import academy.devdojo_maratonajava.javacore.Aintroduçãoclasses.domain.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        carro.nome = "Celta";
+        carro.modelo = "LT Flex";
+        carro.ano = 2014;
+        Carro carro1 = new Carro();
+        carro1.nome = "Corolla";
+        carro1.modelo = "LTZ";
+        carro1.ano = 2020;
+        System.out.println(carro.nome + " " + carro.modelo + " " + carro.ano);
+        System.out.println("\n####################");
+        System.out.println("\n"+carro1.nome + " " + carro1.modelo+ " " + carro1.ano);
+    }
+}
---
+++ src/academy/devdojo_maratonajava/javacore/Aintroduçãoclasses/test/ProfessorTest01.java
@@ -0,0 +1,14 @@
+package academy.devdojo_maratonajava.javacore.Aintroduçãoclasses.test;
+
+import academy.devdojo_maratonajava.javacore.Aintroduçãoclasses.domain.Professor;
+
+public class ProfessorTest01 {
+    public static void main(String[] args) {
+        Professor professor = new Professor();
+        professor.nome = "Mestre Kame";
+        professor.idade = 140;
+        professor.sexo = 'M';
+        System.out.println("Nome: " + professor.nome + "\nIdade: " + professor.idade + "\nSexo: " + professor.sexo + ".");
+    }
+    // PRINCÍPIO DE COESÃO: De preferência um propósito por classe.
+}
