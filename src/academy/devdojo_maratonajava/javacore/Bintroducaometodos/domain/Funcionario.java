7d42225830c6c1833ba49e526b707d35f21a6bc0
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2024-06-30 02:03:42 -0300

    getters and setters update

--- src/academy/devdojo_maratonajava/javacore/Bintroducaometodos/domain/Funcionario.java
+++ src/academy/devdojo_maratonajava/javacore/Bintroducaometodos/domain/Funcionario.java
@@ -1,9 +1,10 @@
 package academy.devdojo_maratonajava.javacore.Bintroducaometodos.domain;
 
 public class Funcionario {
-    public String nome;
-    public int idade;
-    public double[] salarios;
+    private String nome;
+    private int idade;
+    private double[] salarios;
+    private double media;
 
     public void imprime() {
         System.out.println(this.nome);
@@ -27,5 +28,34 @@ public class Funcionario {
             }
             media /= salarios.length;
             System.out.println("Média salarial: " + media);
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public void setSalarios(double[] salarios) {
+        this.salarios = salarios;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double[] getSalarios() {
+        return salarios;
+    }
+
+    public double getMedia() {
+        return media;
     }
 }
--- src/academy/devdojo_maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
+++ src/academy/devdojo_maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -5,10 +5,9 @@ import academy.devdojo_maratonajava.javacore.Bintroducaometodos.domain.Funcionar
 public class FuncionarioTest01 {
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
-        funcionario.nome = "Isaac";
-        funcionario.idade = 23;
-        funcionario.salarios = new double[]{1200, 987.32, 2000};
-
+        funcionario.setNome("Isaac");
+        funcionario.setIdade(23);
+        funcionario.setSalarios(new double[]{1200, 987.32, 2000});
         funcionario.imprime();
         funcionario.imprimeMediaSalarial();
     }
