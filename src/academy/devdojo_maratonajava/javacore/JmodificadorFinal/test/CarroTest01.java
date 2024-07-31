ca567bd6d07dbb289b314c2aec0794edb1186c90
Isaac Meneses <isaac.meneses.trabalhos@gmail.com>
2024-07-31 01:54:14 -0300

    Modificador final pt 01

---
+++ src/academy/devdojo_maratonajava/javacore/JmodificadorFinal/test/CarroTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo_maratonajava.javacore.JmodificadorFinal.test;
+
+import academy.devdojo_maratonajava.javacore.JmodificadorFinal.domain.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
